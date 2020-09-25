38285e3abab9161bb6d7349b5df47c42dcd73994
Tima <onlyosuplay@gmail.com>
2020-09-25 16:17:07 +0300

    new dataset CIFAR10

--- ml_source_2.py
+++ ml_source_2.py
@@ -6,7 +6,7 @@ import torchvision.transforms as transforms
 import numpy as np
 
 from torch.autograd import Variable
-from torchvision.datasets import MNIST
+from torchvision.datasets import CIFAR10
 
 
 class net(nn.Module):
@@ -15,16 +15,17 @@ class net(nn.Module):
         super(net, self).__init__()
 
         # 1. Dummy
-        # 28x28 24x24 12x12 10x10 5x5
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        # 28x28 24x24 12x12 8x8 5x5
+        # 32x32 28x28 14x14 10x10 5x5
+        self.conv1 = nn.Conv2d(3, 10, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        self.lc = nn.Linear(4 * 4 * 64, 10)
+        self.conv2 = nn.Conv2d(10, 30, 5)
+        self.lc = nn.Linear(5 * 5 * 30, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 4 * 4 * 64)
+        x = x.view(-1, 5 * 5 * 30)
         x = self.lc(x)
 
         return x
@@ -43,9 +44,13 @@ if __name__ == "__main__":
 
     trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
 
-    train_set = MNIST(root='./data', train=True, download=True, transform=trans)
-    test_set = MNIST(root='./data', train=False, download=True, transform=trans)
+    # train_set = MNIST(root='./data', train=True, download=True, transform=trans)
+    # test_set = MNIST(root='./data', train=False, download=True, transform=trans)
 
+    train_set = CIFAR10(root='./data', train=True, download=True, transform=trans)
+    test_set = CIFAR10(root='./data', train=False, download=True, transform=trans)
+
+    # CIFAR10 выборка
     train_loader = torch.utils.data.DataLoader(
         dataset=train_set,
         batch_size=batch_size,
@@ -64,6 +69,10 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             x, target = Variable(x), Variable(target)
             # one hot encode
+            # CrossEntropyWithLogits
+            # NLL
+            # BCE
+            # BCEWithLogitsLoss
             # target = F.one_hot(target)  # works w/o this
 
             y = model(x)
@@ -89,7 +98,9 @@ if __name__ == "__main__":
             correct_cnt += (pred_label == target.data).sum()
 
             if batch_idx % 100 == 0:
-                print('==>>> epoch: {}, index: {}, acc: {:.3f}, correct: {}, total: {}'.format(
+                print('==>>> epoch: {}, index: {}, acc: {:.2f}, correct: {}, total: {}'.format(
                     epoch, batch_idx, (correct_cnt * 1.) / total_cnt, correct_cnt, total_cnt))
+
+    torch.save(model.state_dict(), "my-model")
 
     print("Done!")
